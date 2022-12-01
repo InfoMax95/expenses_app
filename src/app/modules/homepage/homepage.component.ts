509f290ac7ce68f290dba7905d918ca18210ecfa
Massimiliano Gasaro <massimilianogasaro95@gmail.com>
2022-12-02 00:21:56 +0100

    update homepage/ change mockup

--- src/app/modules/homepage/homepage.component.ts
+++ src/app/modules/homepage/homepage.component.ts
@@ -8,17 +8,11 @@ import { AuthService } from 'src/app/core/auth/auth.service';
 })
 export class HomepageComponent implements OnInit {
 
-  // isLogged: boolean = true;
-
   constructor(private authService: AuthService) {
 
   }
 
   ngOnInit(): void {
     // this.isLogged = this.authService.getIsLoggedIn();
-  }
-
-  Logout() {
-    //this.authService.isLoggedIn = false;
   }
 }
